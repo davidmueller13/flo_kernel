60fb994d165cbf6a6acd5fb9aa86dad384c2095f
Daniel Pooley <daniel.pooley@gmail.com>
2013-09-16 22:22:09 +1200

    msm: Reduce Max BW to 533MHz

--- arch/arm/mach-msm/asustek/flo/board-flo-gpu.c
+++ arch/arm/mach-msm/asustek/flo/board-flo-gpu.c
@@ -168,13 +168,13 @@ static struct msm_bus_vectors grp3d_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(5336),
+		.ib = KGSL_CONVERT_TO_MBPS(4264),
 	},
 	{
 		.src = MSM_BUS_MASTER_GRAPHICS_3D_PORT1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(5336),
+		.ib = KGSL_CONVERT_TO_MBPS(4264),
 	},
 };
 
